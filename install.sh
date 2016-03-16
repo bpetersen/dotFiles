7ab182df52c24f2a8e1709f128dfad7f9a8b828b
Barton A Petersen <bart.petersen@gmail.com>
2016-03-16 04:45:58 -0600

    Add .eslint

--- install.sh
+++ install.sh
@@ -2,4 +2,4 @@
 ln -s ~/Repos/dotfiles/init.vim ~/.config/nvim/init.vim
 ln -s ~/Repos/dotfiles/bash_profile ~/.bash_profile
 ln -s ~/Repos/dotfiles/tern-project ~/.tern-project
-
+ln -s ~/Repos/dotFiles/eslintrc ~/.eslintrc
