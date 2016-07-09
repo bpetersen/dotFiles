7ef54c258fa86de499d555729e9f5ecb4b5ea56d
Barton A Petersen <bart.petersen@gmail.com>
2016-07-08 21:46:30 -0600

    symlink tmux conf

--- install.sh
+++ install.sh
@@ -3,3 +3,4 @@ ln -s ~/Repos/dotfiles/init.vim ~/.config/nvim/init.vim
 ln -s ~/Repos/dotfiles/bash_profile ~/.bash_profile
 ln -s ~/Repos/dotfiles/tern-project ~/.tern-project
 ln -s ~/Repos/dotFiles/eslintrc ~/.eslintrc
+ln -s ~/Repos/dotFiles/tmux.conf ~/.tmux.conf
