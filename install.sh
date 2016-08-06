af8dad46201f2dd8e80dff55d7def03168bafd53
Barton A Petersen <bart.petersen@gmail.com>
2016-08-06 11:22:48 -0600

    add symlink to bashrc

--- install.sh
+++ install.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 ln -s ~/Repos/dot-files/init.vim ~/.config/nvim/init.vim
 ln -s ~/Repos/dot-files/bash_profile ~/.bash_profile
+ln -s ~/Repos/dot-files/bashrc ~/.bashrc
 ln -s ~/Repos/dot-files/tern-project ~/.tern-project
 ln -s ~/Repos/dot-files/eslintrc ~/.eslintrc
 ln -s ~/Repos/dot-files/tmux.conf ~/.tmux.conf
