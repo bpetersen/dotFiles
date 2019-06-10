ac2e7f3cb43cb49843da83ab1165be6883c21b2d
Barton A Petersen <bart.petersen@gmail.com>
2019-06-10 00:29:46 -0600

    add init script to install all dev dependencies

---
+++ init.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+/usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+
+brew install neovim
+brew install tmux
+brew install git bash-completion
+brew install bash-git-prompt
+brew install the_silver_searcher
+brew install python
+
+pip3 install pynvim
+
+# Node Version Manager (NVM)
+mkdir ~/.nvm
+curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.34.0/install.sh | bash
+. ~/.nvm/nvm.sh
+nvm install node
+
+# Vim - Plug
+curl -fLo ~/.local/share/nvim/site/autoload/plug.vim --create-dirs \
+    https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
+
+mkdir -p ~/.config
+mkdir -p ~/.config/nvim
+
+./symlink-dot-files.sh
+
+nvim +PlugInstall +UpdateRemotePlugins +qa
+
+echo "Close your terminal and reopen."
--- symlink-dot-files.sh
+++ symlink-dot-files.sh
@@ -3,7 +3,5 @@ cwd=$(pwd)
 ln -s $cwd/init.vim ~/.config/nvim/init.vim
 ln -s $cwd/bash_profile ~/.bash_profile
 ln -s $cwd/bashrc ~/.bashrc
-ln -s $cwd/tern-project ~/.tern-project
 ln -s $cwd/eslintrc ~/.eslintrc
 ln -s $cwd/tmux.conf ~/.tmux.conf
-ln -s $cwd/tmuxinator.bash ~/.tmuxinator.bash
