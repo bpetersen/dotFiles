e957ea7bb347b476bfc041419b70dd75694bf3f2
Bart Petersen <bart.petersen@gmail.com>
2023-03-08 11:35:34 -0700

    move to zsh and use powerlevel10k

--- init.sh
+++ init.sh
@@ -1,35 +1,65 @@
 #!/bin/bash
-/usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
 
-brew install neovim
-brew install tmux
-brew install git bash-completion
-brew install bash-git-prompt
-brew install the_silver_searcher
-brew install python
-brew install reattach-to-user-namespace
-
-pip3 install pynvim
-
-# Node Version Manager (NVM)
-mkdir ~/.nvm
-curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.34.0/install.sh | bash
-. ~/.nvm/nvm.sh
-nvm install node
-npm install -g eslint
-npm install -g eslint-plugin-react
-npm install -g prettier
-
-# Vim - Plug
-curl -fLo ~/.local/share/nvim/site/autoload/plug.vim --create-dirs \
-    https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
-
+# ***************************
+# ******  dot files *********
+# ***************************
+#
+# Create config directories
 mkdir -p ~/.config
 mkdir -p ~/.config/nvim
 mkdir -p ~/.config/prettier
 
 ./symlink-dot-files.sh
 
-nvim +PlugInstall +UpdateRemotePlugins +qa
+# ***************************
+# ****** Brew Install *******
+# ***************************
+
+/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+
+brew upgrade
+brew tap Homebrew/bundle
+brew bundle
+brew cleanup
+
+# The rest is in Brewfile
+
+# ********************
+# ****** Python ******
+# ********************
+
+# Python global packages
+pip3 install pynvim
+
+# ********************
+# ****** NodeJS ******
+# ********************
+
+# Node Version Manager (NVM)
+mkdir ~/.nvm
+curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.3/install.sh | bash
+. ~/.nvm/nvm.sh
+nvm install node
+
+# NPM global packages
+npm install -g eslint
+npm install -g eslint-plugin-react
+npm install -g prettier
+npm install -g neovim
+
+# Vim - Plug
+#curl -fLo ~/.local/share/nvim/site/autoload/plug.vim --create-dirs \
+    #https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
+
+
+#nvim +PlugInstall +UpdateRemotePlugins +qa
+
+# ***************************
+# ****** Configure Zsh ******
+# ***************************
+
+# install oh-my-zsh
+# install powerlevel10k
+git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/themes/powerlevel10k
 
 echo "Close your terminal and reopen."
--- symlink-dot-files.sh
+++ symlink-dot-files.sh
@@ -1,8 +1,11 @@
 #!/bin/bash
 cwd=$(pwd)
-ln -s $cwd/init.vim ~/.config/nvim/init.vim
+ln -s $cwd/init.lua ~/.config/nvim/init.lua
 ln -s $cwd/bash_profile ~/.bash_profile
 ln -s $cwd/bashrc ~/.bashrc
 ln -s $cwd/eslintrc ~/.eslintrc
 ln -s $cwd/tmux.conf ~/.tmux.conf
 ln -s $cwd/prettier-config.yml ~/.config/prettier/config.yml
+ln -s $cwd/zshrc ~/.zshrc
+ln -s $cwd/p10k.zsh ~/.p10k.zsh
+ln -s $cwd/Brewfile ~/Brewfile
